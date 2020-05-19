1fb0ade0b71f6e10b5455cb7829e931285e93101
hamin <ggamini7@gmail.com>
2020-05-19 21:30:48 +0900

    upload fibonacci

---
+++ src/MIPS_aseembly_file/fibonacci.s
@@ -0,0 +1,32 @@
+main:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+addi $a0, $0, 4
+jal fibo
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+
+fibo:
+addi $sp, $sp, -12
+sw $s0, 0($sp)
+sw $s1, 4($sp)
+sw $ra, 8($sp)
+slti $t0, $a0, 2
+beq $t0, $0, ELSE
+add $v0, $0, $a0 jr $ra
+
+ELSE:
+addi $s0, $a0, 0
+addi $a0, $a0, -1
+jal fibo
+addi $s1, $v0, 0
+addi $a0, $s0, -2
+jal fibo
+add $s1, $s1, $v0
+j EXIT
+
+EXIT:
+lw $s0, 0($sp)
+lw $s1, 4($sp) lw $ra, 8($sp)
+addi $sp, $sp, 12
+jr $ra 
